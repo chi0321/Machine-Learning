48c1bc827619976dca3dcfbe14fad7af451b67f1
chi0321 <zhangc0321@gmail.com>
2017-08-31 09:59:28 +0800

    add Logistic-Rregression python code

---
+++ Logistic-Regression/test.py
@@ -0,0 +1,48 @@
+import csv
+import random 
+import numpy
+import logistic_regression as LR
+
+#import data
+#data = numpy.genfromtxt('input.csv', delimiter=',')
+# response is in the first column
+Y = numpy.formfile("ex4x.dat")
+X = numpy.formfile("ex4y.dat")
+
+# n-fold 
+nfold = 10
+foldSize = int(m / nfold)
+
+# arrage to store training and testing error
+trainErr = [0.0] * nfold
+testErr = [0.0] * nfold
+allIndex = range(0,m)
+
+for i in range(0, nfold):
+	
+	testIndex = range((foldSize * i), foldSize * (i + 1))
+	trainIndex = list(set(allIndex) - set(testIndex))
+	
+	trainX = X[trainIndex, :]
+	trainY = Y[trainIndex]
+	testX = X[testIndex, :]
+	testY = Y[testIndex]
+	
+	# set parameter
+	alpha = 0.05
+	lam = 0.1
+	model = LR.LogisticRegression(trainX, trainY, alpha, lam)
+	model.run(400, printIter=False)
+	
+	trainPred = model.predict(trainX)
+	trainErr[i] = float(sum(trainPred != trainY)) / len(trainY)
+	
+	testPred = model.predict(testX)
+	testErr[i] = float(sum(testPred != testY)) / len(testY)
+	
+	print("train error=", trainErr[i],"test error=", testErr[i])
+	print(" ")
+
+print("summary:")
+print("average train error = ", numpy.mean(trainErr) * 100, "%")
+print("average test error = ", numpy.mean(testErr) * 100, "%")
